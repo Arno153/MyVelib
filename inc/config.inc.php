7965771fa680cf0e47952a0ece71291bfe891e0f
arno152153 <arnaud@philibert.info>
2018-06-27 21:03:13 +0200

    externalisation de la config mysql

---
+++ inc/config.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+	//DB parameters - prod	
+	$server = 'server';
+	$user = "user";
+	$password = "password";
+	$db = "velib";
+	
+	//DB parameters - dev
+	/*
+	$server = '127.0.0.1';
+	$user = "root";
+	$password = "";
+	$db = "velib";	
+	*/
+?>
--- inc/mysql.inc.php
+++ inc/mysql.inc.php
@@ -1,24 +1,9 @@
 <?php
 
+	include "config.inc.php";
+
 	function mysqlConnect()
-	{
-		
-		//DB parameters - prod
-		
-		$server = 'server';
-		$user = "user";
-		$password = "password";
-		$db = "velib";
-		
-		//DB parameters - dev
-		/*
-		$server = '127.0.0.1';
-		$user = "root";
-		$password = "";
-		$db = "velib";	
-		*/
-		
-		
+	{		 
 		//DB connect
 		@$link = mysqli_connect($server, $user, $password, $db);
 		if (!$link) {
