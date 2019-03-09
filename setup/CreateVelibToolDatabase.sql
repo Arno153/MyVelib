f708dcf29130d7b498e620859902e1eec4dd395d
Arno153 <arnaud@philibert.info>
2019-03-09 18:15:52 +0100

    mise à jour script création db

--- setup/CreateVelibToolDatabase.sql
+++ setup/CreateVelibToolDatabase.sql
@@ -1,9 +1,7 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.3
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
---
--- Généré le :  Sam 23 Juin 2018 à 17:49
-
+--									 
 
 --
 -- Base de données :  `velib`
@@ -30,7 +28,11 @@ CREATE TABLE `velib_activ_station_stat` (
   `networkNbBike` int(11) DEFAULT NULL,
   `networkNbBikeOverflow` int(11) DEFAULT NULL,
   `networkEstimatedNbBike` int(11) DEFAULT NULL,
-  `networkEstimatedNbBikeOverflow` int(11) DEFAULT NULL
+  `networkEstimatedNbBikeOverflow` int(11) DEFAULT NULL,
+  `networkNbEBike` int(11) DEFAULT NULL,
+  `networkNbEBikeOverflow` int(11) DEFAULT NULL,
+  `networkEstimatedNbEBike` int(11) DEFAULT NULL,
+  `networkEstimatedNbEBikeOverflow` int(11) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
@@ -42,7 +44,20 @@ CREATE TABLE `velib_activ_station_stat` (
 DROP TABLE IF EXISTS `velib_api_sanitize`;
 CREATE TABLE `velib_api_sanitize` (
   `JsonDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `JsonMD5` varchar(32)  NOT NULL
+  `JsonMD5` varchar(32) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `velib_nbrlocation`
+--
+
+DROP TABLE IF EXISTS `velib_nbrlocation`;
+CREATE TABLE `velib_nbrlocation` (
+  `date` date NOT NULL,
+  `heure` int(11) NOT NULL,
+  `nbRetraitVAE` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
@@ -109,27 +124,24 @@ CREATE TABLE `velib_station` (
 
 DROP TABLE IF EXISTS `velib_station_min_velib`;
 CREATE TABLE `velib_station_min_velib` (
-  `stationCode` varchar(10) NOT NULL,
-  `stationStatDate` date NOT NULL,
-  `stationVelibMinVelib` int(11) NOT NULL,
-  `stationVelibMaxVelib` int(11) NOT NULL DEFAULT '0',
-  `stationVelibMinVelibOverflow` int(11) DEFAULT NULL,
-  `stationVelibMaxVelibOverflow` int(11) DEFAULT NULL,
-  `updateDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
-
-CREATE TABLE `velib_station_min_velib` (
   `stationCode` varchar(10) COLLATE latin1_general_ci NOT NULL,
   `stationStatDate` date NOT NULL,
-  `stationVelibMinVelib` int(11) NOT NULL,
-  `stationVelibMaxVelib` int(11) NOT NULL DEFAULT '0',
-  `stationVelibMinVelibOverflow` int(11) DEFAULT NULL,
-  `stationVelibMaxVelibOverflow` int(11) DEFAULT NULL,
+  `stationVelibMinVelib` int(11) NOT NULL COMMENT 'velib + vae',
+  `stationVelibMinEVelib` int(11) DEFAULT NULL COMMENT 'vae',
+  `stationVelibMaxVelib` int(11) NOT NULL DEFAULT '0' COMMENT 'velib + vae',
+  `stationVelibMinVelibOverflow` int(11) DEFAULT NULL COMMENT 'velib + vae',
+  `stationVelibMinEVelibOverflow` int(11) DEFAULT NULL COMMENT 'vae',
+  `stationVelibMaxVelibOverflow` int(11) DEFAULT NULL COMMENT 'velib + vae',
   `stationVelibExit` int(11) DEFAULT '0',
   `stationEVelibExit` int(11) DEFAULT NULL,
   `updateDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
+PARTITION BY RANGE COLUMNS(stationStatDate)
+(
+PARTITION p0 VALUES LESS THAN ('2018-12-31') ENGINE=InnoDB,
+PARTITION p1 VALUES LESS THAN (MAXVALUE) ENGINE=InnoDB
+);
+
 -- --------------------------------------------------------
 
 --
@@ -162,6 +174,12 @@ ALTER TABLE `velib_api_sanitize`
   ADD KEY `JsonMD5` (`JsonMD5`);
 
 --
+-- Index pour la table `velib_nbrlocation`
+--
+ALTER TABLE `velib_nbrlocation`
+  ADD PRIMARY KEY (`date`,`heure`);
+
+--
 -- Index pour la table `velib_network`
 --
 ALTER TABLE `velib_network`
@@ -185,8 +203,8 @@ ALTER TABLE `velib_station_min_velib`
 -- Index pour la table `velib_station_status`
 --
 ALTER TABLE `velib_station_status`
-  ADD KEY `id` (`id`,`stationState`);  
-  
+  ADD PRIMARY KEY (`id`,`stationStatusDate`),
+  ADD KEY `id` (`id`,`stationState`);
   
 --
 -- AUTO_INCREMENT pour les tables exportées
@@ -197,9 +215,9 @@ ALTER TABLE `velib_station_status`
 --
 ALTER TABLE `velib_network`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT pour la table `velib_station`
 --
 ALTER TABLE `velib_station`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
