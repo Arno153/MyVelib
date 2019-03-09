24fbb4537f875f1e441b0d05a429d1f5e4fa2fc5
Arno153 <arnaud@philibert.info>
2019-03-09 19:09:24 +0100

    url du menu en relatif

--- inc/menu.inc.php
+++ inc/menu.inc.php
@@ -1,16 +1,16 @@
-<link rel="stylesheet" media="all" href="./../css/newMenu.css?<?php echo filemtime('./css/newMenu.css');?>">
+<link rel="stylesheet" media="all" href="./css/newMenu.css?<?php echo filemtime('./css/newMenu.css');?>">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
 <div id="cssmenu"><div id="menu-button">Menu</div>
   <ul>
-     <li><a href="https://velib.philibert.info/"><i class="fa fa-fw fa-bicycle"></i> Accueil</a></li>
-     <li><a href="https://velib.philibert.info/carte-des-stations.php"><i class="fa fa-fw fa-leanpub"></i> Carte</a></li>
+     <li><a href="./"><i class="fa fa-fw fa-bicycle"></i> Accueil</a></li>
+     <li><a href="./carte-des-stations.php"><i class="fa fa-fw fa-leanpub"></i> Carte</a></li>
      <li class="has-sub"><span class="submenu-button"></span><a href="#">Plus de cartes</a>
         <ul>
-           <li><a href="https://velib.philibert.info/liste-des-stations.php"><i class="fa fa-fw fa-bars"></i> Liste des stations</a></li>
-           <li><a href="https://velib.philibert.info/carte-des-mouvements.php"><i class="fa fa-fw fa-map-o"></i> Carte des mouvements</a></li>
-           <li><a href="https://velib.philibert.info/carte-heatmap.php"><i class="fa fa-fw fa-map-o"></i> Heat Map</a></li>
-		   <li><a href="https://velib.philibert.info/carte-des-velib-bloques.php"><i class="fa fa-fw fa-map-o"></i> Carte des velib bloqués</a></li>
+           <li><a href="./liste-des-stations.php"><i class="fa fa-fw fa-bars"></i> Liste des stations</a></li>
+           <li><a href="./carte-des-mouvements.php"><i class="fa fa-fw fa-map-o"></i> Carte des mouvements</a></li>
+           <li><a href="./carte-heatmap.php"><i class="fa fa-fw fa-map-o"></i> Heat Map</a></li>
+		   <li><a href="./carte-des-velib-bloques.php"><i class="fa fa-fw fa-map-o"></i> Carte des velib bloqués</a></li>
         </ul>
      </li>
      <li class="has-sub"><span class="submenu-button"></span><a href="#">Contact et autres</a>
