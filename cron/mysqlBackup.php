e752c0b45731ed9f9c518ef69724b83180ed74b9
Arno153 <arnaud@philibert.info>
2020-11-14 17:32:07 +0100

    Sauvegarde BDD - ajout compression gz et purge auto

--- cron/mysqlBackup.php
+++ cron/mysqlBackup.php
@@ -1,10 +1,33 @@
 <?php
 
 include "./../inc/mysql.inc.php";
-echo "DB Backup Begin<br>";
-backupDatabaseTables("velib_");
-echo "DB Backup End";
+include "./../inc/cacheMgt.inc.php";
+echo "Début sauvegarde BDD<br>";
+$debug = 1; //debug mode
+velibAPIParser_SetDbBackupLock(); //disable updates during backup
+$sourceFile = backupDatabaseTables($debug, "velib_"); //backup
+velibAPIParser_RemoveDbBackupLock();//enable updates after backup
+echo "Fin de la sauvegarde BDD<br><br>";
 
+// compression de la sauvegarde
+echo "Début de la compression de la sauvegarde BDD<br>";
+if($debug == 1)error_log( "memory used ".memory_get_usage()/1048576);
+$gz = gzcompressfile($sourceFile);
+if($gz!= false)
+{
+	unlink ($sourceFile);
+	echo "--> succès<br>";
+}
+if($debug == 1)error_log( "memory used ".memory_get_usage()/1048576);
+echo "Fin de la compression de la sauvegarde BDD<br><br>";
+
+if($gz != false)
+{
+echo "debut de la purge des anciennes sauvegarde<br>";
+$i = purgeSQLBackup(7,30,$debug);
+echo "-->".$i." fichier(s) purgé(s)<br>";
+echo "fin de la purge des anciennes sauvegarde<br>";
+}
 
 /**
  * @function    backupDatabaseTables
@@ -12,7 +35,7 @@ echo "DB Backup End";
  * @link        http://www.codexworld.com
  * @usage       Backup database tables and save in SQL file
  */
-function backupDatabaseTables($tables = '*'){
+function backupDatabaseTables($debug, $tables = '*'){
     //connect & select the database
 	$db = mysqlConnect();
 	
@@ -39,14 +62,15 @@ function backupDatabaseTables($tables = '*'){
     }
 
 	$fileTime = time();
+	$fileName = './../backup_files/db-backup-'.$fileTime.'.sql';
 	//open file
-	$handle = fopen('./../backup_files/db-backup-'.$fileTime.'.sql','w+');	
+	$handle = fopen($fileName,'w+');	
 	
     //loop through the tables
     foreach($tables as $table)
 	{
-		//echo "table: ".$table."<br>";
-		//echo "memory used ".memory_get_usage()."<br>";
+		if($debug == 1)error_log( "backup table: ".$table);
+		if($debug == 1)error_log( "memory used ".memory_get_usage()/1048576);
         $result = $db->query("SELECT * FROM $table");
         $numColumns = $result->field_count;
 
@@ -76,10 +100,10 @@ function backupDatabaseTables($tables = '*'){
 			if($i==25000) //ecriture fichier toute les 100k lignes pour limiter la conso mémoire
 			{
 				//write to file
-				//echo "memory used ".memory_get_usage()."<br>";
+				if($debug == 1)error_log( "memory used ".memory_get_usage()/1048576);
 				fwrite($handle,$return);
-				//"table: ".$table." partially saved to file <br><br>";
-				//echo "memory used ".memory_get_usage()."<br>";				
+				if($debug == 1)error_log( "table: ".$table." partially saved to file ");
+				if($debug == 1)error_log( "memory used ".memory_get_usage()/1048576);		
 				$return = "";
 				$i=0;				
 			}		
@@ -93,9 +117,89 @@ function backupDatabaseTables($tables = '*'){
 		$return = "";
 		
 		// echo "memory used ".memory_get_usage()."<br>";
-		// echo "table: ".$table." saved to file <br><br>";		
+		echo "--> table: ".$table." exportée<br>";		
     }
 	
 	//close file
 	fclose($handle);
+	//close db connection
+	mysqlClose($db);
+	
+	if($debug == 1)error_log("max memory used: ".memory_get_peak_usage()/1048576);
+	return $fileName;
 }
+
+function purgeSQLBackup($delaiPurge, $delaiPurgeTotale, $debug)
+{
+	$nbFichierPurges = 0;
+	$folderName = './../backup_files/';
+	date_default_timezone_set("Europe/Paris");
+	setlocale(LC_TIME, 'fr_FR');
+	
+	//contenu du dossier
+	$files1 = scandir($folderName);
+	
+	//traitement du contenu du dossier
+	foreach ($files1 as $key => $value)
+	{
+		if (!in_array($value,array(".","..","index.html"))) //éliminer certain fichiers/dossiers
+		{
+			$comment='';
+			$datefichier = filemtime ( $folderName.'/'.$value );
+			$interval = date_diff(new DateTime(date("Y-m-d H:i:s",$datefichier)), new DateTime("now") );	
+			$nbJourFichier = intval($interval->format('%a'));
+	
+			
+			if($nbJourFichier > $delaiPurgeTotale)
+			{
+				$comment = " > $delaiPurgeTotale j : purge inconditionnelle";
+				unlink ($folderName.$value);
+				$nbFichierPurges = $nbFichierPurges +1;
+			}
+			else if($nbJourFichier > $delaiPurge)
+			{
+				$comment = $comment." > $delaiPurge j";
+				$comment = " > $delaiPurge j: purge sauf 1/$delaiPurge :";
+				$comment = $comment." floatval-intval: ".(floatval($nbJourFichier/$delaiPurge)-intval($nbJourFichier/$delaiPurge)).": ";
+				if((floatval($nbJourFichier/$delaiPurge)-intval($nbJourFichier/$delaiPurge))!=0)
+				{
+					$comment = $comment."purge!";
+					unlink ($folderName.$value);
+					$nbFichierPurges = $nbFichierPurges +1;
+				}
+			}
+			else $comment = $comment." <= $delaiPurge j: on garde!";
+			
+			if($debug == 1)
+				error_log(
+					$value
+					." : "
+					.date ("F d Y H:i:s",$datefichier)
+					." : "
+					.$nbJourFichier
+					." : "
+					.$comment
+						  );
+		}
+	} 
+
+	return $nbFichierPurges;
+}
+
+function gzcompressfile($source,$level=false){
+    $dest=$source.'.gz';
+    $mode='wb'.$level;
+    $error=false;
+    if($fp_out=gzopen($dest,$mode)){
+        if($fp_in=fopen($source,'rb')){
+            while(!feof($fp_in))
+                gzwrite($fp_out,fread($fp_in,1024*512));
+            fclose($fp_in);
+            }
+          else $error=true;
+        gzclose($fp_out);
+        }
+      else $error=true;
+    if($error) return false;
+      else return $dest;
+    } 
