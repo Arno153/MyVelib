c105abc45ba83feacf2bcd5eac22a29f4be4fec1
Arno153 <arnaud@philibert.info>
2023-09-04 19:38:58 +0200

    jquery from 3.7.0 to 3.7.1

--- networkBikeRatio.php
+++ networkBikeRatio.php
@@ -671,8 +671,11 @@
 	</div>
 
 	<!-- graph to full screen -->
-	<script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
-	
+	<script
+				  src="https://code.jquery.com/jquery-3.7.1.min.js"
+				  integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
+				  crossorigin="anonymous"></script>	
+				  
 	<script>
 
 		var button_GraphEvolutionNombreEVelib = 1;
